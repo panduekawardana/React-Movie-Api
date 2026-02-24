f61dfd186102c7c321af22ac0fdc15de517dd197
panduekawardana <panduwardanav@gmail.com>
2026-02-25 00:03:07 +0800

    Search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,22 +1,22 @@
-import React from "react";
+import React from 'react';
 
 const Search = ({ searchTerm, setSearchTerm }) => {
-  return (
-    <div className="search">
-      <div>
-        <img src="search.svg" alt="search" className="text-white h-8" />
-        <input
-          type="text"
-          placeholder="Search your movie here!"
-          autoFocus
-          value={searchTerm}
-          onChange={(e) => {
-            setSearchTerm(e.target.value);
-          }}
-        ></input>
-      </div>
-    </div>
-  );
+    return (
+        <div className="search">
+            <div>
+                <img src="search.svg" alt="search" className="text-white h-8" />
+                <input
+                    type="text"
+                    placeholder="Search your movie here!"
+                    autoFocus
+                    value={searchTerm}
+                    onChange={(e) => {
+                        setSearchTerm(e.target.value);
+					}}>
+				</input>
+            </div>
+        </div>
+    );
 };
 
 export default Search;
